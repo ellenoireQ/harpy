a08347dc7597acd24de78eeecbf336a4e1a26595
ellenoireQ <ketchupnaiso@gmail.com>
2026-03-27 08:24:49 +0700

    compiler: Add documentation for compiler_error, compiler_warning, and compiler_info macros

--- src/compiler/hc/logs/print.rs
+++ src/compiler/hc/logs/print.rs
@@ -1,3 +1,8 @@
+/// Emits a compiler error diagnostic for `span` with message `msg`.
+///
+/// This macro is `#[macro_export]`, so it can be called from other crates.
+/// The `$crate` prefix ensures the internal path resolves to this crate even
+/// when the macro is invoked externally.
 #[macro_export]
 macro_rules! compiler_error {
     ($span:expr, $msg:expr) => {
@@ -9,6 +14,10 @@ macro_rules! compiler_error {
     };
 }
 
+/// Emits a compiler warning diagnostic for `span` with message `msg`.
+///
+/// Uses `$crate` so the path to `logs::diagnostics` remains robust when the
+/// macro is expanded from outside this crate.
 #[macro_export]
 macro_rules! compiler_warning {
     ($span:expr, $msg:expr) => {
@@ -20,6 +29,9 @@ macro_rules! compiler_warning {
     };
 }
 
+/// Emits a compiler note/info diagnostic for `span` with message `msg`.
+///
+/// This maps to `DiagnosticKind::Note` in the diagnostics backend.
 #[macro_export]
 macro_rules! compiler_info {
     ($span:expr, $msg:expr) => {
